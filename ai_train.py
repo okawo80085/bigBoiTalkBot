5a58e4bfcf6d1a691db92311721ae9338a592f99
okawo <okawo.198@gmail.com>
2019-06-26 12:37:24 +0300

    first complete model draft
    had to change input shape to 1D tensor bc of the GRU cell type

--- ai_train.py
+++ ai_train.py
@@ -1,11 +1,11 @@
-from tflearn.data_utils import to_categorical
 import tensorflow as tf
 import tensorflow.keras as ker
 from tensorflow.keras import layers as l
+from tensorflow.keras.callbacks import TensorBoard as tb
 
 import numpy as np
-import os
 import re
+import time
 import utils
 
 print (tf.__version__)
@@ -14,13 +14,19 @@ def make_model():
 	model = ker.Sequential()
 
 	# input
-	model.add(l.Dense(256, use_bias=False, input_shape=(2, 200, 95)))
+	model.add(l.Dense(2*200, use_bias=False, input_shape=(2*200,)))
 	model.add(l.BatchNormalization())
 	model.add(l.LeakyReLU())
-	model.add(l.Flatten())
+	#model.add(l.Flatten())
 	
+	model.add(l.Reshape((2, 200)))
+	assert model.output_shape == (None, 2, 200)
 
 	# hidden layers
+	model.add(l.GRU(64, return_sequences=True, activation='relu'))
+	model.add(l.GRU(32, return_sequences=True, activation='relu'))
+	model.add(l.GRU(32, return_sequences=True, activation='relu'))
+	model.add(l.GRU(16, return_sequences=True, activation='relu'))
 
 
 	# output
@@ -33,5 +39,10 @@ generator_model = make_model()
 
 generator_model.summary()
 
+generator_model.compile(optimizer=tf.train.AdamOptimizer(0.00003),
+	loss='mse',
+	metrics=['accuracy'])
+
+tb_callback = tb(log_dir='log/{}'.format(time.time()))
 
 print ('(っ・ω・）っ≡≡≡≡≡≡☆')
