21aa0dc70c78bbd7cc241f7d1da210e6bd8efdac
okawo <okawo.198@gmail.com>
2019-06-27 17:44:23 +0300

    minor update
    but also, IT CAN TALK NOW... KINDA...

--- ai_chat.py
+++ ai_chat.py
@@ -1,11 +1,6 @@
 import tensorflow as tf
-import tensorflow.keras as ker
 from tensorflow.keras.models import load_model
 
-import numpy as np
-import re
-import time
-import os
 import utils
 
 print (tf.__version__)
@@ -19,6 +14,8 @@ except Exception as e:
 	print (e)
 	raise Exception('failed to load model from the file \'{}\''.format(modelSaveFileName))
 
+botModel.summary()
+
 vocab = sorted([chr(i) for i in range(32, 127) if i != 96])
 vocab.insert(0, None)
 
@@ -30,7 +27,7 @@ while 1:
 	resp, ix, ix_prob = utils.generate_a_reply(botModel, usrInput, vocab)
 
 	print (ix)
-	print (ix_prob)
+	#print (ix_prob)
 	print ([''.join(resp).strip(' ')])
 
 print ('(っ・ω・）っ≡≡≡≡≡≡☆')
