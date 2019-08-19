7d696817c3d2dc85cac92c457c853525016aa3a6
okawo <okawo.198@gmail.com>
2019-08-19 14:32:26 +0300

    minor change

--- train-onstartup.sh
+++ train-onstartup.sh
@@ -8,11 +8,13 @@ echo gnome-terminal -- \"$PWD/$name\"
 
 cd $path
 
-for (( c=0; c<5; c++))
+for (( c=0; c<8; c++))
 do
 	python3 ai_train.py
 	git commit ./modelz/ytc_adopted_bpe_edition.h5 -m "automated training"
 	git push origin master
+	echo
 	echo batch $c complete
+	echo
 done
 echo done
