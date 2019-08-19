efa92eccb3e4bbc6b87cb9073c9d9746321d3961
okawo <okawo.198@gmail.com>
2019-08-19 14:14:43 +0300

    minorchanges, oh and only works on linux

--- train-onstartup.sh
+++ train-onstartup.sh
@@ -11,6 +11,8 @@ cd $path
 for (( c=0; c<5; c++))
 do
 	python3 ai_train.py
+	git commit ./modelz/ytc_adopted_bpe_edition.h5 -m "automated training"
+	git push origin master
 	echo batch $c complete
 done
 echo done
